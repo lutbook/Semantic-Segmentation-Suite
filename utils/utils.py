9b15dbea4f02128d5285b643be0004940c6a9b67
lutbook <lutbuk@gmail.com>
2021-02-15 22:18:44 +0900

    dataset preparation

--- utils/utils.py
+++ utils/utils.py
@@ -21,23 +21,29 @@ def prepare_data(dataset_dir):
     test_input_names=[]
     test_output_names=[]
     for file in os.listdir("/" + dataset_dir + "/train"):
-        cwd = os.getcwd()
-        train_input_names.append(cwd + "/" + dataset_dir + "/train/" + file)
+        # cwd = os.getcwd()
+        # train_input_names.append(cwd + "/" + dataset_dir + "/train/" + file)
+        train_input_names.append("/" + dataset_dir + "/train/" + file)
     for file in os.listdir("/" + dataset_dir + "/train_labels"):
-        cwd = os.getcwd()
-        train_output_names.append(cwd + "/" + dataset_dir + "/train_labels/" + file)
+        # cwd = os.getcwd()
+        # train_output_names.append(cwd + "/" + dataset_dir + "/train_labels/" + file)
+        train_output_names.append("/" + dataset_dir + "/train_labels/" + file)
     for file in os.listdir("/" + dataset_dir + "/val"):
-        cwd = os.getcwd()
-        val_input_names.append(cwd + "/" + dataset_dir + "/val/" + file)
+        # cwd = os.getcwd()
+        # val_input_names.append(cwd + "/" + dataset_dir + "/val/" + file)
+        val_input_names.append("/" + dataset_dir + "/val/" + file)
     for file in os.listdir("/" + dataset_dir + "/val_labels"):
-        cwd = os.getcwd()
-        val_output_names.append(cwd + "/" + dataset_dir + "/val_labels/" + file)
+        # cwd = os.getcwd()
+        # val_output_names.append(cwd + "/" + dataset_dir + "/val_labels/" + file)
+        val_output_names.append("/" + dataset_dir + "/val_labels/" + file)
     for file in os.listdir("/" + dataset_dir + "/test"):
-        cwd = os.getcwd()
-        test_input_names.append(cwd + "/" + dataset_dir + "/test/" + file)
+        # cwd = os.getcwd()
+        # test_input_names.append(cwd + "/" + dataset_dir + "/test/" + file)
+        test_input_names.append("/" + dataset_dir + "/test/" + file)
     for file in os.listdir("/" + dataset_dir + "/test_labels"):
-        cwd = os.getcwd()
-        test_output_names.append(cwd + "/" + dataset_dir + "/test_labels/" + file)
+        # cwd = os.getcwd()
+        # test_output_names.append(cwd + "/" + dataset_dir + "/test_labels/" + file)
+        test_output_names.append("/" + dataset_dir + "/test_labels/" + file)
     train_input_names.sort(),train_output_names.sort(), val_input_names.sort(), val_output_names.sort(), test_input_names.sort(), test_output_names.sort()
     return train_input_names,train_output_names, val_input_names, val_output_names, test_input_names, test_output_names
 
