2c5bf74feafe9489e6fe35586b9e07c38d39281f
lutbook <lutbuk@gmail.com>
2021-02-16 02:38:00 +0900

    dataset prep

--- utils/utils.py
+++ utils/utils.py
@@ -23,6 +23,9 @@ def prepare_data(dataset_dir):
     for file in os.listdir("/" + dataset_dir + "/train"):
         # cwd = os.getcwd()
         # train_input_names.append(cwd + "/" + dataset_dir + "/train/" + file)
+        if file.split("_")[0] == '.':
+            print(file)
+            continue
         train_input_names.append("/" + dataset_dir + "/train/" + file)
     for file in os.listdir("/" + dataset_dir + "/train_labels"):
         # cwd = os.getcwd()
@@ -34,18 +37,30 @@ def prepare_data(dataset_dir):
     for file in os.listdir("/" + dataset_dir + "/val"):
         # cwd = os.getcwd()
         # val_input_names.append(cwd + "/" + dataset_dir + "/val/" + file)
+        if file.split("_")[0] == '.':
+            print(file)
+            continue
         val_input_names.append("/" + dataset_dir + "/val/" + file)
     for file in os.listdir("/" + dataset_dir + "/val_labels"):
         # cwd = os.getcwd()
         # val_output_names.append(cwd + "/" + dataset_dir + "/val_labels/" + file)
+        if file.split("_")[0] == '.':
+            print(file)
+            continue
         val_output_names.append("/" + dataset_dir + "/val_labels/" + file)
     for file in os.listdir("/" + dataset_dir + "/test"):
         # cwd = os.getcwd()
         # test_input_names.append(cwd + "/" + dataset_dir + "/test/" + file)
+        if file.split("_")[0] == '.':
+            print(file)
+            continue
         test_input_names.append("/" + dataset_dir + "/test/" + file)
     for file in os.listdir("/" + dataset_dir + "/test_labels"):
         # cwd = os.getcwd()
         # test_output_names.append(cwd + "/" + dataset_dir + "/test_labels/" + file)
+        if file.split("_")[0] == '.':
+            print(file)
+            continue
         test_output_names.append("/" + dataset_dir + "/test_labels/" + file)
     train_input_names.sort(),train_output_names.sort(), val_input_names.sort(), val_output_names.sort(), test_input_names.sort(), test_output_names.sort()
     return train_input_names,train_output_names, val_input_names, val_output_names, test_input_names, test_output_names
